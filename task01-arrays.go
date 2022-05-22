f0f12e4db840da50b745eb34d5cf3c50b09ef272
tarugatto <talgat.zhaimenov@gmail.com>
2022-05-22 18:18:46 +0600

    Finished 'task01-arrays'

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,14 @@
 package homework
 
+func homework() {
+	var arr = [15]float32{4.5, 8, 3, 7, 1, 6.4, 8, 4, 1, 3, 11, 7.1, 5, 9.6, 1}
+	average(arr)
+}
+
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	sum := 0
+	for i := 0; i < len(input); i++ {
+		sum += i
+	}
+	return float32(sum)
 }
