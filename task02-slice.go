c6f11d59f1d0a1090710ee5c5e29c5a1a517fb75
tarugatto <talgat.zhaimenov@gmail.com>
2022-05-23 12:44:37 +0600

    Finished 'task02-slice'

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,17 @@
 package homework
 
+func homework() {
+	var avrg = []int64{23, 45, 74, 92, 34, 66}
+	slc1 := avrg[2:5]
+	reverse(slc1)
+}
+
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	var slc2 []int64
+
+	for i := len(input) - 1; i >= 0; i-- {
+		slc2 = append(slc2, input[i])
+	}
+
+	return slc2
 }
