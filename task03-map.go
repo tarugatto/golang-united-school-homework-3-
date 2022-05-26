4b0809d236f4af6bab391974250b2e7b6f0a2603
tarugatto <talgat.zhaimenov@gmail.com>
2022-05-26 20:34:10 +0600

    Modified 'task03-map'. Range and +1

--- task03-map.go
+++ task03-map.go
@@ -4,11 +4,13 @@ var mapy = map[int]string{5: "Kim", 2: "Jimmy", 4: "Sara", 1: "Emily", 3: "Steve
 var _ = sortMapValues(mapy)
 
 func sortMapValues(input map[int]string) (result []string) {
-	arr1 := make([]string, len(input)) //empty slice with input lenght
+	slc := make([]string, len(input)+1) //empty slice with input lenght
 
 	for i, elmnt := range input {
-		arr1[i-1] = elmnt
+		slc[i] = elmnt
 	}
 
-	return arr1
+	slc1 := slc[1:]
+
+	return slc1
 }
